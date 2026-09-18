65c1484f142030f1fbdb296ecff5982fef2a2dad
adernomary <maris.aderno@gmail.com>
2026-09-18 11:55:58 -0300

    finalizar

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Rodape() {
+    return(
+        <>
+        <h1>footer</h1>
+        <p>2026 - desenvolvido por Mariana Aderno</p>
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './components/Home'
+import Home from './components/Home'
+import Rodape from './components/Footer'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Home />
+    <Rodape />
     <App />
   </StrictMode>,
 )
